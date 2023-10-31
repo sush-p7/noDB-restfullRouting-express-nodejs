502acf063f776bfaeafd2edd094ebca2a7590bf7
sush <sushh.p.007@gmail.com>
2023-11-01 03:05:08 +0530

    adding new features and (improving design and logic) add new ejs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const path = require('path')
 const app = express()
 
@@ -14,14 +15,49 @@ let posts = [
         time : '3:40 pm',
         date : 'Feb 24, 2022'
 
+    },
+    {
+        postid: '3',
+        name: 'Jhon Doe',
+        username: '@jhondoe',
+        postMessage: "Why do they call it 'debugging' when it feels more like 'wildly guessing and hoping for the best'?",
+        tags: ['#programming', '#coding', '#debugging'],
+        likes : 0,
+        time : '3:40 pm',
+        date : 'Feb 24, 2022'
+
     }
 ]
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('views path', path.join(__dirname, 'views'));
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'resources')));
+app.set('views path', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-app.get('/posts', (req, res) => {
+app.get('/posts', (req, res) => { 
+    // console.log(posts);
     res.render('posts/index', { i ,posts});
+})
+app.get('/posts/:postid',(req,res)=>{ 
+    const {postid} = req.params;
+    // console.log(postid);
+    const post = posts.find(p => p.postid === postid);
+    // console.log({post});
+    res.render('posts/show',{post});
+    
+});
+
+// app.get('/comments/:id', (req, res) => {
+//     const { id } = req.params;
+//     const comment = comments.find(c => c.id === id);
+//     res.render('comments/show', { comment })
+// })
+
+app.get('/posts/:postid/edit',(req,res)=>{
+    const { postid} = req.params;
+    const post = posts.find(p=> p.postid === postid);
+    console.log({postid});
+    res.render('posts/edit',{post});
 })
 app.listen(3000, () => {
     console.log('listening on 3000')
