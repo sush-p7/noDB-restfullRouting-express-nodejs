0030c575a56c91515d7504bbb8ade77dd8779bb5
sush <sushh.p.007@gmail.com>
2023-11-01 23:02:08 +0530

    completed the restFull routing project with no database feature offering such as CURD - Create Update Read Delete

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid'); //For generating ID's
 const path = require('path')
 const app = express()
-
+ 
 let i = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let posts = [
     {
@@ -34,9 +35,16 @@ app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'resources')));
 app.set('views path', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+
 app.get('/posts', (req, res) => { 
     // console.log(posts);
     res.render('posts/index', { i ,posts});
+})
+app.get('/posts/new',(req,res)=>{
+    // console.log('new post')
+    res.render('posts/newPost')
+    // res.render('posts/newPost');
 })
 app.get('/posts/:postid',(req,res)=>{ 
     const {postid} = req.params;
@@ -53,12 +61,41 @@ app.get('/posts/:postid',(req,res)=>{
 //     res.render('comments/show', { comment })
 // })
 
+
+
 app.get('/posts/:postid/edit',(req,res)=>{
+    console.log(req);
     const { postid} = req.params;
     const post = posts.find(p=> p.postid === postid);
     console.log({postid});
     res.render('posts/edit',{post});
 })
+app.delete('/posts/:postid',(req,res)=>
+{   console.log("delete post has been deleted");
+    const {postid} = req.params
+    posts = posts.filter(p=>p.postid!=postid)
+    res.redirect('/posts')
+})
+app.patch('/posts/:postid',(req,res)=>{
+    const { postid } = req.params;
+    const oldPostMessage = posts.find(p=> p.postid === postid);
+    const newPostMessage = req.body.newPostMassage
+    console.log({newPostMessage});
+    oldPostMessage.postMessage = newPostMessage
+    res.redirect('/posts')
+})
+app.post('/posts',(req,res)=>{
+    console.log("new post has been created");
+    const {postUsername}= req.body;
+    const {postMessage} = req.body
+    const {postName} = req.body
+    posts.push({postid:uuid(),name:postName,username:postUsername,postMessage:postMessage,tags: ['#programming', '#coding', '#debugging'],
+    likes : 0,
+    time : '3:40 pm',
+    date : 'Feb 24, 2022'})
+    res.redirect('/posts');
+})
+
 app.listen(3000, () => {
     console.log('listening on 3000')
 })
